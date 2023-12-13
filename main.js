5e7bd3473bfe91a58bcfa4de9f10dbd026dbdd3c
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2023-12-13 17:41:28 +0500

    ^ tasi bajarilkdi

---
+++ main.js
@@ -0,0 +1,76 @@
+
+
+//  1 masala
+// let n = +prompt(`Istalgan sonni kiriting`);
+// let counter = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 3 == 0 && i % 7 == 0) {
+//         counter++
+//     }
+// }
+// console.log(counter);
+
+//  2 masala
+// let sum = 0;
+// for (let n = +prompt(`Istalgan sonni kiriting`); n >= 1; n--) {
+//     if (n % 2 == 0) {
+//         sum = sum + n;
+//     }
+// }
+// console.log(sum);
+
+//  3 masala
+
+//  4 masala
+
+
+//  5 masala
+
+//  6 masala
+// let num1 = +prompt(`Istalgan sonni kiriting`)
+
+// for(let i = 1; i <= num1; i++) {
+//     if(num1 % i == 0){
+//         // document.write(i);
+//         console.log(i);
+//     }
+// }
+
+
+// 7 masala 
+// let number = +prompt(`Sonni kiriting`)
+// let counter = 0
+// for(let num = 1; num <= number; num++) {
+//     if(number % num == 0){
+//         counter = counter + 1
+//     }
+// }
+// console.log(counter);
+
+//  8 masala
+// let number = +prompt(`Sonni kiriting`)
+// let counter = 0;
+// for(let num = 1; num <= number; num++) {
+//     if(number % num == 0){
+//         counter = counter + 1;
+//     }
+// }
+// if(counter == 2) {
+//     console.log(`tub son`);
+// }
+
+
+
+
+
+//  9 masala
+// let sum = 0;
+// let n = 3;
+// for (let i = 1; i <= n; i++) {
+//       sum = sum + (i * 2);
+// }
+// console.log(sum)
+
+
+//  10 masala
+
